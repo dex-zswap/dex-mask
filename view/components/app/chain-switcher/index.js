4bfbe97d478900ab5daf00184039c8e589f1d9b1
rwson <rw_song@sina.com>
2021-12-21 11:19:13 +0800

    chain switcher

--- view/components/app/chain-switcher/index.js
+++ view/components/app/chain-switcher/index.js
@@ -34,231 +34,39 @@ class ChainSwitcher extends Component {
   static contextTypes = {
     t: PropTypes.func,
   };
+
   defaultChains = DEFAULT_NETWORK_LIST.map(({ chainId, provider, label }) => {
     return {
       key: chainId,
-      value: provider,
+      value: chainId,
+      provider: provider,
       isBulitIn: true,
-      render: (item) => <SelectorOption {...item} />,
       label,
     };
   });
-  triggerEl = null;
-  state = {
-    showMenu: false,
-  };
-
-  switchNetWork(providerType) {
-    const { provider, setProviderType } = this.props;
-    setProviderType(providerType);
-  }
-
-  handleClick(newProviderType) {
-    const { setProviderType } = this.props;
-    this.toggleNetworkDrop();
-    setProviderType(newProviderType);
-  }
-
-  toggleNetworkDrop() {
-    this.setState((prev) => {
-      return { ...prev, showMenu: !prev.showMenu };
-    });
-  }
-
-  getNetworkName() {
-    const { provider } = this.props;
-    const providerName = provider.type;
-    let name;
-
-    if (providerName === 'mainnet') {
-      name = this.context.t('mainnet');
-    } else if (providerName === 'ropsten') {
-      name = this.context.t('ropsten');
-    } else if (providerName === 'kovan') {
-      name = this.context.t('kovan');
-    } else if (providerName === 'rinkeby') {
-      name = this.context.t('rinkeby');
-    } else if (providerName === 'goerli') {
-      name = this.context.t('goerli');
-    } else {
-      name = provider.nickname || this.context.t('unknownNetwork');
-    }
-
-    return name;
-  }
-
-  renderCurrentChain() {
-    return (
-      <div className="chain-switcher__current-chain">
-        {this.getNetworkName()}
-      </div>
-    );
-  }
-
-  renderDefaultChains() {
-    const { provider, setProviderType } = this.props;
-    const providerName = provider.type;
-    return (
-      <div className="chain-switcher__default-chains base-width">
-        {/* <div
-        className={classnames([
-        'chain-switcher__default-chain-item',
-        providerName === DEX_MAINNET && 'chain-switcher__current-chain',
-        ])}
-        onClick={() => this.switchNetWork(DEX_MAINNET)}
-        >
-        DEX
-        </div>
-        <div
-        className={classnames([
-        'chain-switcher__default-chain-item',
-        providerName === MAINNET && 'chain-switcher__current-chain',
-        ])}
-        onClick={() => this.switchNetWork(MAINNET)}
-        >
-        ETH
-        </div>
-        <div
-        className={classnames([
-        'chain-switcher__default-chain-item',
-        providerName === BSC_MAINNET && 'chain-switcher__current-chain',
-        ])}
-        onClick={() => this.switchNetWork(BSC_MAINNET)}
-        >
-        BSC
-        </div> */}
-      </div>
-    );
-  }
-
-  renderCustomRpcListMenus(rpcListDetail, provider) {
-    const reversedRpcListDetail = rpcListDetail.slice().reverse();
-    return reversedRpcListDetail.map((entry) => {
-      const { rpcUrl, chainId, ticker = 'ETH', nickname = '' } = entry;
-      const isCurrentRpcTarget =
-        provider.type === NETWORK_TYPE_RPC && rpcUrl === provider.rpcUrl;
-      return (
-        <MenuItem
-          className={isCurrentRpcTarget ? 'active' : ''}
-          key={`common${rpcUrl}`}
-          onClick={() => {
-            this.props.hideNetworkDropdown();
-
-            if (isPrefixedFormattedHexString(chainId)) {
-              this.props.setRpcTarget(rpcUrl, chainId, ticker, nickname);
-            } else {
-              this.props.displayInvalidCustomNetworkAlert(nickname || rpcUrl);
-            }
-
-            this.toggleNetworkDrop();
-          }}
-        >
-          <div className="network__chain-icon"></div>
-          <div
-            className={
-              isCurrentRpcTarget
-                ? 'chain-name chain-checked-name'
-                : 'chain-name'
-            }
-          >
-            <LongLetter text={nickname || rpcUrl} length={13} />
-          </div>
-          {isCurrentRpcTarget && (
-            <div className="chain-checked">
-              <img width="7px" src="images/dex/account-menu/checked.png" />
-            </div>
-          )}
-        </MenuItem>
-      );
-    });
-  }
-
-  renderNetworkMenuItem(network) {
+ 
+  getNetWorkOptions() {
+    const networkOptions = this.defaultChains;
     const {
-      provider: { type: providerType },
+      frequentRpcListDetail,
     } = this.props;
-    const iconBg =
-      'dexMainnet' === network
-        ? '/images/dex-token.png'
-        : 'mainnet' === network
-        ? '/images/eth_logo.png'
-        : 'bscMainnet' === network
-        ? '/images/bnb.png'
-        : '/images/dex/settings/chain-icon.png';
-    return (
-      <MenuItem
-        className={network === providerType ? 'active' : ''}
-        key={network}
-        onClick={() => this.handleClick(network)}
-      >
-        <div
-          style={{
-            background: `url(${iconBg}) no-repeat
-            center / cover`,
-          }}
-          className="network__chain-icon"
-        ></div>
-        <div
-          className={
-            network === providerType
-              ? 'chain-name chain-checked-name'
-              : 'chain-name'
-          }
-        >
-          <LongLetter text={this.context.t(network)} length={13} />
-        </div>
-        {network === providerType && (
-          <div className="chain-checked">
-            <img width="7px" src="images/dex/account-menu/checked.png" />
-          </div>
-        )}
-      </MenuItem>
-    );
-  }
 
-  renderNetWorkMenu() {
-    const {
-      provider: { rpcUrl: activeNetwork },
-      setNetworksTabAddMode,
-      setSelectedSettingsRpcUrl,
-      triggerEl,
-      hideNetworkDropdown,
-    } = this.props;
-    const rpcListDetail = this.props.frequentRpcListDetail;
-    return (
-      <Menu
-        className="network-droppo__menu"
-        anchorElement={this.triggerEl}
-        onHide={() => this.toggleNetworkDrop()}
-      >
-        {this.renderNetworkMenuItem('dexMainnet')}
-        {this.renderNetworkMenuItem('mainnet')}
-        {this.renderNetworkMenuItem('bscMainnet')}
-        {this.renderNetworkMenuItem('ropsten')}
-        {this.renderNetworkMenuItem('kovan')}
-        {this.renderNetworkMenuItem('rinkeby')}
-        {this.renderNetworkMenuItem('goerli')}
-        {this.renderCustomRpcListMenus(rpcListDetail, this.props.provider)}
-        <div className="network-divder"></div>
-        <MenuItem
-          onClick={() => {
-            this.props.history.push(NETWORKS_FORM_ROUTE);
-            setSelectedSettingsRpcUrl('');
-            setNetworksTabAddMode(true);
-          }}
-        >
-          <div className="network__chain-icon custom"></div>
-          <div className="chain-name">{this.context.t('customRPC')}</div>
-        </MenuItem>
-      </Menu>
-    );
+    return networkOptions.concat(frequentRpcListDetail.map((item) => {
+      return {
+        key: item.chainId,
+        value: item.chainId,
+        isBulitIn: false,
+        label: item.nickname,
+        setPrcParams: [item.rpcUrl, item.chainId, item.ticker, item.nickname]
+      };
+    }));
   }
 
   switchNetWork = (value, detail) => {
     if (detail.isBulitIn) {
-      this.props.setProviderType(value);
+      this.props.setProviderType(detail.provider);
       return;
-    } // rpcUrl, chainId, ticker, nickname
+    }
 
     this.props.setRpcTarget.apply(null, detail.setPrcParams ?? []);
   };
@@ -268,18 +76,18 @@ class ChainSwitcher extends Component {
       history,
       showNetworkDropdown,
       provider,
-      frequentRpcListDetail,
       resetNetworksForm,
       addRpc,
     } = this.props;
-    const { showMenu } = this.state;
-    const networkOptions = this.defaultChains;
+
+    const networkOptions = this.getNetWorkOptions();
+
     return (
       <>
         <div className="chain-switcher">
           <Selector
             className="chain-switcher-selector"
-            selectedValue={provider.type}
+            selectedValue={provider.chainId}
             options={networkOptions}
             labelRender={SelectorOption}
             itemRender={SelectorOption}
@@ -293,15 +101,16 @@ class ChainSwitcher extends Component {
                     history.push(NETWORKS_FORM_ROUTE);
                   }}
                 >
-                  {this.context.t('addNetwork')}
-                  <i className="add-icon"></i>
+                  <span>
+                    {this.context.t('addNetwork')}
+                    <i className="add-icon"></i>
+                  </span>
                 </Button>
               ) : null
             }
             small
           />
         </div>
-        {/* {showMenu && this.renderNetWorkMenu()} */}
       </>
     );
   }
